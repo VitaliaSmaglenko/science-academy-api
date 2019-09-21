09d1f4c9d39da9c63f4972ec3e6b8223950554a7
VitaliaSmaglenko <vitaliasmaglenko@gmail.com>
2019-09-21 19:44:54 +0300

    Create service and dto for user

---
+++ app/Services/Transformers/RequestToUserTransformer.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace App\Services\Transformers;
+
+use Illuminate\Http\Request;
+use App\Services\Dto\UserDto;
+
+class RequestToUserTransformer
+{
+    public function transform(Request $request): UserDto
+    {
+        return new UserDto(
+            $request->name,
+            $request->surname,
+            $request->patronymic,
+            $request->email,
+            $request->password,
+            $request->science_degree,
+            $request->academic_rank
+        );
+    }
+}
