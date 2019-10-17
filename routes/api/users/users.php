74521a86d254c3710f77d772295b64d2a63d121e
VitaliaSmaglenko <vitaliasmaglenko@gmail.com>
2019-10-17 19:44:31 +0300

    Rename routes

--- routes/api/users/users.php
+++ routes/api/users/users.php
@@ -1,41 +1,41 @@
 <?php
 
 Route::post('users', [
-    'as' => 'post.create.user',
+    'as' => 'post.user.create',
     'uses' => 'Api\UserManageController@create'
 ])->middleware("auth:api")->middleware("manage-user");
 
 Route::delete('users/{id}', [
-    'as' => 'delete.delete.user',
+    'as' => 'delete.user.delete',
     'uses' => 'Api\UserManageController@delete'
 ])->middleware("auth:api")->middleware("manage-user");
 
 Route::get('users', [
-    'as' => 'get.get-collection.user',
+    'as' => 'get.user.get-all',
     'uses' => 'Api\UserManageController@getAll'
 ])->middleware("auth:api")->middleware("manage-user");
 
 Route::get('users/{id}', [
-    'as' => 'get.get.user',
+    'as' => 'get.user.get',
     'uses' => 'Api\UserManageController@get'
 ])->middleware("auth:api")->middleware("manage-user");
 
 Route::put('users/{id}', [
-    'as' => 'put.update.user',
+    'as' => 'put.user.update',
     'uses' => 'Api\UserManageController@update'
 ])->middleware("auth:api")->middleware("manage-user");
 
 Route::put('users/{id}/password', [
-    'as' => 'put.update-password.user',
+    'as' => 'put.user.update-password',
     'uses' => 'Api\UserManageController@updatePassword'
 ])->middleware("auth:api")->middleware("manage-user");
 
 Route::put('users/{id}/role', [
-    'as' => 'put.update-role.user',
+    'as' => 'put.user.update-role',
     'uses' => 'Api\UserManageController@updateRole'
 ])->middleware("auth:api")->middleware("manage-user");
 
 Route::put('users/{id}/department', [
-    'as' => 'put.add-department.user',
+    'as' => 'put.user.add-department',
     'uses' => 'Api\UserManageController@addDepartment'
 ])->middleware("auth:api")->middleware("manage-user");
