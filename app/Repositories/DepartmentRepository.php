e0c775695301336395fa61c66a3da4766c4585da
VitaliaSmaglenko <vitaliasmaglenko@gmail.com>
2019-10-19 16:30:58 +0300

    Update repository

--- app/Repositories/DepartmentRepository.php
+++ app/Repositories/DepartmentRepository.php
@@ -11,4 +11,27 @@ class DepartmentRepository
     {
         return Department::get();
     }
+
+    public function getById(int $id): ?Department
+    {
+        return Department::where('id', $id)->first();
+    }
+
+    public function getInfo(int $id): ?Department
+    {
+        return Department::where('id', $id)
+            ->with(['users' => function($query) use ($id) {
+                $query->where('department_id', $id);
+            }])
+            ->first();
+    }
+
+    public function getByUser(int $id, int $userId): ?Department
+    {
+        return Department::where('id', $id)
+            ->with(['users' => function($query) use ($userId) {
+                $query->where('user_id', $userId);
+            }])
+            ->first();
+    }
 }
