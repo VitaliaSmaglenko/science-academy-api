4edda6846d1d91f2aa7e0b8cf5b7476d87b7bac1
VitaliaSmaglenko <vitaliasmaglenko@gmail.com>
2019-11-24 00:49:56 +0200

    Update repository and service

--- app/Services/Work/CompletedWorkService.php
+++ app/Services/Work/CompletedWorkService.php
@@ -23,4 +23,23 @@ class CompletedWorkService
 
         return $completedWork;
     }
+
+    public function delete(CompletedWork $completedWork): bool
+    {
+        return $completedWork->delete();
+    }
+
+    public function update(CompletedWorkDto $completedWorkDto, CompletedWork $completedWork): CompletedWork
+    {
+        $completedWork->work_id = $completedWorkDto->getWorkId();
+        $completedWork->title = $completedWorkDto->getTitle();
+        $completedWork->reference = $completedWorkDto->getReference();
+        $completedWork->co_author_id = $completedWorkDto->getCoAuthorId();
+        $completedWork->number_of_hours = $completedWorkDto->getNumberOfHours();
+        $completedWork->season = $completedWorkDto->getSeason();
+
+        $completedWork->save();
+
+        return $completedWork;
+    }
 }
