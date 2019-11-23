a7ef6324cb5d83fca4635040578b8592f13b0ebc
VitaliaSmaglenko <vitaliasmaglenko@gmail.com>
2019-11-23 16:59:43 +0200

    Create migration

---
+++ database/migrations/2019_11_23_141558_create_completed_works_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompletedWorksTable extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('completed_works', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('type_id');
+            $table->string("title");
+            $table->string("reference");
+            $table->unsignedInteger('co-author_id');
+            $table->double("number_of_hours");
+            $table->enum("season", ["Осінь", "Весна"]);
+            $table->timestamps();
+        });
+
+        Schema::table('completed_works', function ($table) {
+            $table->engine = 'InnoDB';
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('co-author_id')->references('id')->on('users')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('type_id')->references('id')->on('works')
+                ->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('completed_works');
+    }
+}
