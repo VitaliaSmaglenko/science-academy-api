f845066dc533cad4ba6ef4b59b9d08e90d18fe27
VitaliaSmaglenko <vitaliasmaglenko@gmail.com>
2019-11-23 21:51:15 +0200

    Create requset

---
+++ app/Http/Requests/CreateWorkRequest.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateWorkRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'type_id' => ['required', 'string',],
+            'title' => ['required', 'string'],
+            'reference' => ['string'],
+            'co_author_id' => ['string'],
+            'number_of_hours' => ['required', 'string'],
+            'season' => ['required', 'string'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'type_id.required' => 'Вид роботи не вибраний',
+            'title.required' => 'Інформація о роботі порожня',
+            'number_of_hours.required' => 'Кількість часів не обрано',
+            'season.required' => 'Поле сезону порожнє',
+            'string' => 'Невалідна строка',
+        ];
+    }
+}
