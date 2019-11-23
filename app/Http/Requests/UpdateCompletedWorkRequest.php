8c62c4ff9611abdd48accddcaf2e8e4bc852c4f8
VitaliaSmaglenko <vitaliasmaglenko@gmail.com>
2019-11-24 00:51:49 +0200

    Create request for update work

---
+++ app/Http/Requests/UpdateCompletedWorkRequest.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateCompletedWorkRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'work_id' => ['required', 'string',],
+            'title' => ['required', 'string'],
+            'reference' => ['string'],
+            'co_author_id' => ['string'],
+            'number_of_hours' => ['required', 'string'],
+            'season' => ['required', 'string'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'work_id.required' => 'Вид роботи не вибраний',
+            'title.required' => 'Інформація о роботі порожня',
+            'number_of_hours.required' => 'Кількість часів не обрано',
+            'season.required' => 'Поле сезону порожнє',
+            'string' => 'Невалідна строка',
+        ];
+    }
+}
