fc011b367e29b68b3a47e8b2ac0942ad743ed054
VitaliaSmaglenko <vitaliasmaglenko@gmail.com>
2019-11-24 18:12:37 +0200

    Add route for indicators

--- routes/api/indicators/indicators.php
+++ routes/api/indicators/indicators.php
@@ -4,3 +4,28 @@ Route::post('indicators', [
     'as' => 'post.indicators.create',
     'uses' => 'Api\IndicatorController@create'
 ])->middleware("auth:api");
+
+Route::get('indicators', [
+    'as' => 'get.indicators.getForUser',
+    'uses' => 'Api\IndicatorController@getForUser'
+])->middleware("auth:api");
+
+Route::get('indicators/{id}', [
+    'as' => 'get.indicators.show',
+    'uses' => 'Api\IndicatorController@show'
+])->middleware("auth:api");
+
+Route::get('indicators/users/{id}', [
+    'as' => 'get.indicators.getByUser',
+    'uses' => 'Api\IndicatorController@getByUser'
+])->middleware("auth:api");
+
+Route::delete('indicators/{id}', [
+    'as' => 'delete.indicators.destroy',
+    'uses' => 'Api\IndicatorController@destroy'
+])->middleware("auth:api");
+
+Route::put('indicators/{id}', [
+    'as' => 'put.indicators.update',
+    'uses' => 'Api\IndicatorController@update'
+])->middleware("auth:api");
