4ca09b6b9bcdec49c2133c26c8325621634fc5d3
VitaliaSmaglenko <vitaliasmaglenko@gmail.com>
2019-11-24 17:22:44 +0200

    Create resource for indicator

---
+++ app/Http/Resources/IndicatorResource.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class IndicatorResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'quantity' => $this->quantity,
+            'season' => $this->season,
+            'number_of_hours' => $this->number_of_hours,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'work' => WorkResource::make($this->works),
+            'user' => UserResource::make($this->users),
+        ];
+    }
+}
